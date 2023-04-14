5ad7590abf2f7c9a4e37823de0b307f8a6d2e9c0
JP Barbosa <jp@jp7.com.br>
2023-04-14 16:10:26 -0300

    Movies People Graph

--- packages/graph/src/movies/index.spec.ts
+++ packages/graph/src/movies/index.spec.ts
@@ -15,6 +15,12 @@ export const expectMoviesToBeEqual = (movie1: Movie, movie2: Movie) => {
   expect(movie1Parsed.title).toEqual(movie2Parsed.title);
   expect(movie1Parsed.tagline).toEqual(movie2Parsed.tagline);
   expect(movie1Parsed.released).toEqual(movie2Parsed.released);
+
+  Object.keys(movie1Parsed.people).forEach((key) => {
+    expect(movie1Parsed.people[key]).toIncludeSameMembers(
+      movie2Parsed.people[key]
+    );
+  });
 };
 
 describe('graph.movies', () => {
@@ -55,6 +61,25 @@ describe('graph.movies', () => {
   it('should update movie tagline', async () => {
     const [movie] = await graph.movies(session).getAll(sampleMovie.title);
     movie.tagline += ' updated';
+    const updatedMovie = await graph.movies(session).update(movie.id, movie);
+    expectMoviesToBeEqual(updatedMovie, movie);
+  });
+
+  it('should add one actor and one reviewer to the movie', async () => {
+    const [movie] = await graph.movies(session).getAll(sampleMovie.title);
+    movie.people.actors.push({
+      name: 'Clancy Brown',
+      born: 1959,
+      roles: ['Captain Andrew Ryan'],
+    });
+    const updatedMovie = await graph.movies(session).update(movie.id, movie);
+    expectMoviesToBeEqual(updatedMovie, movie);
+  });
+
+  it('should remove one actor and one reviewer from the movie', async () => {
+    const [movie] = await graph.movies(session).getAll(sampleMovie.title);
+    movie.people.actors.pop();
+    movie.people.reviewers.pop();
     const updatedMovie = await graph.movies(session).update(movie.id, movie);
     expectMoviesToBeEqual(updatedMovie, movie);
   });
